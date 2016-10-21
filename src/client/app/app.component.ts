cc40344d6f28a5dbb6345a57736d7bd3ead64adf
Landuber Kassa <landuber.kassa@here.com>
2016-10-21 13:38:27 -0700

    Detail view modifications in light of the filter and search changes

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import { JobTypesActions, JobInputsActions } from './shared/actions/index';
+import { JobsActions, JobTypesActions, JobInputsActions } from './shared/actions/index';
 
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
@@ -16,5 +16,6 @@ export default class AppComponent {
   constructor(private _store: Store<any>) {
     this._store.dispatch({type: JobTypesActions.REQUEST_JOBTYPES});
     this._store.dispatch({type: JobInputsActions.REQUEST_JOBINPUTS});
+    this._store.dispatch({type: JobsActions.LOAD_JOBS, payload: { batch: 0 }});
   }
 }
